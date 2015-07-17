d85fdd022439614719e2bb8786e0ab54ba1930ff
Denny Tek <tek.denny@gmail.com>
2015-07-17 14:43:26 -0400

    Add show for single route view

--- NYCapable/app/controllers/routes_controller.rb
+++ NYCapable/app/controllers/routes_controller.rb
@@ -1,7 +1,11 @@
 class RoutesController < ApplicationController
 
   def index
-    @route = Route.all
+    @route = Route.all.order("route_id ASC")
+  end
+
+  def show
+    @route = Route.find(params[:id])
   end
 
 end
