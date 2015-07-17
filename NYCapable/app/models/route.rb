c88ca66c5860a64c45664f7a0fb966439fa907ec
Taylor Hang <sixthand6th@gmail.com>
2015-07-17 15:14:42 -0400

    refactor update method

--- NYCapable/app/models/route.rb
+++ NYCapable/app/models/route.rb
@@ -12,102 +12,55 @@ class Route < ActiveRecord::Base
       "notes" => note_cleaner(@doc.xpath('//text')[i].children.text())
         }
     end
-
   end
 
   def update_status
     @current_status.each do |route|
       if route[0].keys[0] == "123"
-        @route = Route.where(route_short_name: "1").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "2").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "3").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "1"))
+        status_data(Route.where(route_short_name: "2"))
+        status_data(Route.where(route_short_name: "3"))
 
       elsif route[0].keys[0] == "456"
-        @route = Route.where(route_short_name: "4").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "5").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "6").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "4"))
+        status_data(Route.where(route_short_name: "5"))
+        status_data(Route.where(route_short_name: "5X"))
+        status_data(Route.where(route_short_name: "6"))
+        status_data(Route.where(route_short_name: "6X"))
+
 
       elsif route[0].keys[0] == "7"
-        @route = Route.where(route_short_name: "4").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "5").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "6").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "7"))
+        status_data(Route.where(route_short_name: "7X"))
 
       elsif route[0].keys[0] == "ACE"
-        @route = Route.where(route_short_name: "A").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "C").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "E").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "A"))
+        status_data(Route.where(route_short_name: "C"))
+        status_data(Route.where(route_short_name: "E"))
 
       elsif route[0].keys[0] == "BDFM"
-        @route = Route.where(route_short_name: "B").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "D").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "F").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "M").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "B"))
+        status_data(Route.where(route_short_name: "D"))
+        status_data(Route.where(route_short_name: "F"))
+        status_data(Route.where(route_short_name: "M"))
 
       elsif route[0].keys[0] == "G"
-        @route = Route.where(route_short_name: "G").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "G"))
 
       elsif route[0].keys[0] == "JZ"
-        @route = Route.where(route_short_name: "J").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "Z").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "J"))
+        status_data(Route.where(route_short_name: "Z"))
 
       elsif route[0].keys[0] == "L"
-        @route = Route.where(route_short_name: "L").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "L"))
 
       elsif route[0].keys[0] == "NQR"
-        @route = Route.where(route_short_name: "N").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "Q").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
-        @route = Route.where(route_short_name: "R").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "N"))
+        status_data(Route.where(route_short_name: "Q"))
+        status_data(Route.where(route_short_name: "R"))
 
       elsif route[0].keys[0] == "S"
-        @route = Route.where(route_short_name: "S").first.update_attributes(
-          :service_status, route[0].values[0].values[0],
-          :service_status_note, route[0].values[0].values[1])
+        status_data(Route.where(route_short_name: "S"))
       else
         puts "Error"
       end
@@ -118,6 +71,10 @@ class Route < ActiveRecord::Base
 
     def note_cleaner(path)
       path.gsub(/<\/?[^>]+>/, '').gsub("\n", '').gsub("&nbsp;", '').gsub("&#149;", '')
+    end
+
+    def status_data(ar_call)
+      ar_call.first.update_attributes(:service_status, route[0].values[0].values[0], :service_status_note, route[0].values[0].values[1])
     end
 
 end
