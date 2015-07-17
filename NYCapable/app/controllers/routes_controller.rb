5f8833db1c304d97470cc911ab64353bbd4b63fa
Taylor Hang <sixthand6th@gmail.com>
2015-07-17 15:56:09 -0400

    integrate working model method

--- NYCapable/app/controllers/routes_controller.rb
+++ NYCapable/app/controllers/routes_controller.rb
@@ -2,6 +2,7 @@ class RoutesController < ApplicationController
 
   def index
     @route = Route.all.order("route_id ASC")
+    Route.update_status
   end
 
   def show
--- NYCapable/app/models/route.rb
+++ NYCapable/app/models/route.rb
@@ -1,80 +1,74 @@
 class Route < ActiveRecord::Base
   require 'open-uri'
 
-  def initialize
-    @routes = Route.all
-    @doc = Nokogiri::XML(open('http://web.mta.info/status/serviceStatus.txt'))
-    @current_status = {}
-
-    (0..9).each do |i|
-      @current_status[@doc.xpath("//name")[i].children.text()] = {
-      "status" => @doc.xpath('//status')[i].children.text(),
-      "notes" => note_cleaner(@doc.xpath('//text')[i].children.text())
-        }
-    end
+  def self.note_cleaner(path)
+    path.gsub(/<\/?[^>]+>/, '').gsub("\n", '').gsub("&nbsp;", '').gsub("&#149;", '')
   end
 
-  def update_status
+  def self.update_status
+    @routes = Route.all
+
+    @doc = Nokogiri::XML(open('http://web.mta.info/status/serviceStatus.txt'))
+
+    @current_status = []
+
+    (0..9).each do |i|
+      @current_status << [@doc.xpath("//name")[i].children.text() => {
+      "status" => @doc.xpath('//status')[i].children.text(),
+      "notes" => note_cleaner(@doc.xpath('//text')[i].children.text())
+        }]
+    end
+
     @current_status.each do |route|
       if route[0].keys[0] == "123"
-        status_data(Route.where(route_short_name: "1"))
-        status_data(Route.where(route_short_name: "2"))
-        status_data(Route.where(route_short_name: "3"))
+        Route.where(route_short_name: "1").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "2").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "3").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
       elsif route[0].keys[0] == "456"
-        status_data(Route.where(route_short_name: "4"))
-        status_data(Route.where(route_short_name: "5"))
-        status_data(Route.where(route_short_name: "5X"))
-        status_data(Route.where(route_short_name: "6"))
-        status_data(Route.where(route_short_name: "6X"))
+        Route.where(route_short_name: "4").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "5").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "5X").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "6").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "6X").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
 
       elsif route[0].keys[0] == "7"
-        status_data(Route.where(route_short_name: "7"))
-        status_data(Route.where(route_short_name: "7X"))
+        Route.where(route_short_name: "7").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "7X").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
       elsif route[0].keys[0] == "ACE"
-        status_data(Route.where(route_short_name: "A"))
-        status_data(Route.where(route_short_name: "C"))
-        status_data(Route.where(route_short_name: "E"))
+        Route.where(route_short_name: "A").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "C").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "E").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
       elsif route[0].keys[0] == "BDFM"
-        status_data(Route.where(route_short_name: "B"))
-        status_data(Route.where(route_short_name: "D"))
-        status_data(Route.where(route_short_name: "F"))
-        status_data(Route.where(route_short_name: "M"))
+        Route.where(route_short_name: "B").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "D").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "F").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "M").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
       elsif route[0].keys[0] == "G"
-        status_data(Route.where(route_short_name: "G"))
+        Route.where(route_short_name: "G").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
       elsif route[0].keys[0] == "JZ"
-        status_data(Route.where(route_short_name: "J"))
-        status_data(Route.where(route_short_name: "Z"))
+        Route.where(route_short_name: "J").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "Z").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
       elsif route[0].keys[0] == "L"
-        status_data(Route.where(route_short_name: "L"))
+        Route.where(route_short_name: "L").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
       elsif route[0].keys[0] == "NQR"
-        status_data(Route.where(route_short_name: "N"))
-        status_data(Route.where(route_short_name: "Q"))
-        status_data(Route.where(route_short_name: "R"))
+        Route.where(route_short_name: "N").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "Q").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
+        Route.where(route_short_name: "R").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
 
       elsif route[0].keys[0] == "S"
-        status_data(Route.where(route_short_name: "S"))
+        Route.where(route_short_name: "S").first.update_attributes({:service_status => route[0].values[0].values[0], :service_status_note => route[0].values[0].values[1]})
       else
         puts "Error"
       end
     end
   end
-
-  private
-
-    def note_cleaner(path)
-      path.gsub(/<\/?[^>]+>/, '').gsub("\n", '').gsub("&nbsp;", '').gsub("&#149;", '')
-    end
-
-    def status_data(ar_call)
-      ar_call.first.update_attributes(:service_status, route[0].values[0].values[0], :service_status_note, route[0].values[0].values[1])
-    end
 
 end
