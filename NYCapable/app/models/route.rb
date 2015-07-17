e58827658136c8d1ab6b22893625cf59cb55ccce
Taylor Hang <sixthand6th@gmail.com>
2015-07-17 12:07:32 -0400

    add note column to routes

--- NYCapable/app/models/route.rb
+++ NYCapable/app/models/route.rb
@@ -1,3 +1,26 @@
 class Route < ActiveRecord::Base
+  require 'open-uri'
 
+  def initialize
+    @doc = Nokogiri::XML(open('http://web.mta.info/status/serviceStatus.txt'))
+    @current_status = {}
+  end
+
+  def service_status
+    (0..9).each do |i|
+      @current_status[@doc.xpath("//name")[i].children.text()] = {
+      "status" => @doc.xpath('//status')[i].children.text(),
+      "notes" => note_cleaner(@doc.xpath('//text')[i].children.text())
+        }
+    end
+  end
+
+  def update_status
+
+  end
+
+  private
+    def note_cleaner(path)
+      path.gsub(/<\/?[^>]+>/, '').gsub("\n", '').gsub("&nbsp;", '').gsub("&#149;", '')
+    end
 end
--- NYCapable/db/schema.rb
+++ NYCapable/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717144355) do
+ActiveRecord::Schema.define(version: 20150717160531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,10 @@ ActiveRecord::Schema.define(version: 20150717144355) do
     t.string   "route_url"
     t.string   "route_color"
     t.string   "route_text_color"
-    t.string   "service_status",   default: ""
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.string   "service_status",      default: ""
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "service_status_note", default: ""
   end
 
 end
