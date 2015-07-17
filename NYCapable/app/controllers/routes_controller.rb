f6f2670ce3827be5950c3aaed9fb1a2c0230156e
John Doe <benjamin.cohen1989@gmail.com>
2015-07-17 19:10:00 -0400

    added gemfiles and method in controller to update distance_tos, though it will need to be moved eventually

--- NYCapable/app/controllers/routes_controller.rb
+++ NYCapable/app/controllers/routes_controller.rb
@@ -1,6 +1,17 @@
 class RoutesController < ApplicationController
 
   def index
+    @user_lat = 40.706299
+    @user_long = -74.009146
+    @user_location = Geokit::LatLng.new(@user_lat,@user_long)
+
+    @stations = Station.all
+    @stations.each do |station|
+      @station_location = Geokit::LatLng.new(station.latitude,station.longitude)
+      station.distance_to = @user_location.distance_to(@station_location)
+      station.save
+    end
+
     @route = Route.all.order("route_id ASC")
   end
 
