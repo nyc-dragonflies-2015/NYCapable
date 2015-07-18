b07413ad1404a0df211f41eeb77546266a6208ef
Denny Tek <tek.denny@gmail.com>
2015-07-18 17:46:23 -0400

    Add initial tests

--- NYCapable/spec/controllers/routes_spec.rb
+++ NYCapable/spec/controllers/routes_spec.rb
@@ -7,10 +7,10 @@ describe RoutesController do
     get :index
     (expect(response.status).to eq(200))
   end
+
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
     end
-
   end
 end
