d04a38e1244c0c531befa66fba834b14b4644360
Denny Tek <tek.denny@gmail.com>
2015-07-18 17:48:55 -0400

    Add initial tests

---
+++ NYCapable/spec/controllers/other_resources_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe OtherResourcesController do
+
+ describe "GET index" do
+  it 'should get the index' do
+    get :index
+    (expect(response.status).to eq(200))
+end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+end
