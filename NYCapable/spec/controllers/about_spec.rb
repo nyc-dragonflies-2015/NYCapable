b38a12bd824a68eb664bce70b272a63492a383e0
Denny Tek <tek.denny@gmail.com>
2015-07-18 17:48:12 -0400

    Add initial tests

--- NYCapable/spec/controllers/about_spec.rb
+++ NYCapable/spec/controllers/about_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe AboutController do
 
-  describe "GET index" do
-    it 'should get the index' do
-      get :index
-      (expect(response.status).to eq(200))
-    end
 
-    it "renders the :index template" do
+ describe "GET index" do
+  it 'should get the index' do
+    get :index
+    (expect(response.status).to eq(200))
+end
+
+    it "renders the index template" do
       get :index
       expect(response).to render_template("index")
     end
   end
-
 
 end
