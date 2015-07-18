5c21862e33a2049cb7f501ecb118a03439a211bd
John Doe <benjamin.cohen1989@gmail.com>
2015-07-18 11:19:34 -0400

    updated routes controller to pull down closest 5 stations

--- NYCapable/app/controllers/routes_controller.rb
+++ NYCapable/app/controllers/routes_controller.rb
@@ -1,8 +1,8 @@
 class RoutesController < ApplicationController
 
   def index
-    @user_lat = 40.706299
-    @user_long = -74.009146
+    @user_lat = 40.760750
+    @user_long = -73.990438
     @user_location = Geokit::LatLng.new(@user_lat,@user_long)
 
     @stations = Station.all
@@ -11,7 +11,7 @@ class RoutesController < ApplicationController
       station.distance_to = @user_location.distance_to(@station_location)
       station.save
     end
-
+    @closest_stations = Station.order(distance_to: :asc).limit(5)
     @route = Route.all.order("route_id ASC")
   end
 
