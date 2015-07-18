05f6d73278856246cabf1bd4a9316cf6823daea7
Denny Tek <tek.denny@gmail.com>
2015-07-18 17:46:23 -0400

    Add initial tests

--- NYCapable/spec/controllers/routes_spec.rb
+++ NYCapable/spec/controllers/routes_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 describe RoutesController do
 
  describe "GET index" do
-    it "pulls MTA routes from the database" do
-      get :index
-      expect(assigns(:route)).to be_a ActiveRecord::Relation
-    end
+  it 'should get the index' do
+    get :index
+    (expect(response.status).to eq(200))
+end
 
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
     end
-
   end
 end
