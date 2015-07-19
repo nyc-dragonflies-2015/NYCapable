f944fe569a99105433d1111f4ca6c0684be0bc50
Taylor Hang <sixthand6th@gmail.com>
2015-07-19 16:44:33 -0400

    revise test - still need to stub

--- NYCapable/spec/controllers/routes_spec.rb
+++ NYCapable/spec/controllers/routes_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe RoutesController do
 
- describe "GET index" do
-  it 'should get the index' do
-    get :index
-    (expect(response.status).to eq(200))
-end
+  describe "GET index" do
+    it 'should get the index' do
+      get :index
+      (expect(response.status).to eq(200))
+    end
 
     it "renders the index template" do
       get :index
