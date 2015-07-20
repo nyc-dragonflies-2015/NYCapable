41fe2480054ce351a52148c03a3287d1dc4216db
Denny Tek <tek.denny@gmail.com>
2015-07-20 17:52:10 -0400

    Delete factory girl from rails_helper - throwing errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,6 +50,5 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  # Factory-Girl
-  config.include FactoryGirl::Syntax::Methods
+
 end
