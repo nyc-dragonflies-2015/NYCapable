940a0d1145dd374b9272d5bf6bee1a737b7b09c3
julia-castro <julia.castro@outlook.com>
2015-07-20 17:26:49 -0400

    clean up and rename function for autocomplete functionality

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,63 +1,15 @@
+var placeSearch, autocomplete, userInputLocation;
 
 
-function places() {
+function search() {
 
-  var markers = [];
-  var map = new google.maps.Map(document.getElementById('gulia'), {
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+  autocomplete = new google.maps.places.Autocomplete(
+     (document.getElementById('autocomplete')));
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var userInputLocation = [autocomplete.getPlace().geometry.location.lat(),autocomplete.getPlace().geometry.location.lng()]
+    window.location = "http://localhost:3000/routes?variable=" + userInputLocation;
   });
 
-  var defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(-33.8902, 151.1759),
-      new google.maps.LatLng(-33.8474, 151.2631));
-  map.fitBounds(defaultBounds);
-
-  var input = /** @type {HTMLInputElement} */(
-      document.getElementById('pac-input'));
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  debugger
-  var searchBox = new google.maps.places.SearchBox((input));
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-
-
-    if (places.length == 0) {
-      return;
-    }
-    for (var i = 0, marker; marker = markers[i]; i++) {
-      marker.setMap(null);
-    }
-
-    markers = [];
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0, place; place = places[i]; i++) {
-      var image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      var marker = new google.maps.Marker({
-        map: map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location
-      });
-
-      markers.push(marker);
-
-      bounds.extend(place.geometry.location);
-    }
-
-    map.fitBounds(bounds);
-  });
-
-  google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-    searchBox.setBounds(bounds);
-  });
 }
 
-google.maps.event.addDomListener(window, 'ready', places)
