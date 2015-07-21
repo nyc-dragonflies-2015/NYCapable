c46c1c31376c9cd4c390c17d86fba8abcf6f6cda
julia-castro <julia.castro@outlook.com>
2015-07-21 16:02:28 -0400

    add function for returning latlongs for origin and destination

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,5 +1,5 @@
-var placeSearch, autocomplete, userInputLocation;
-
+var placeSearch, autocomplete;
+var userInputLocation
 
 function search() {
 
@@ -25,4 +25,50 @@ function geolocate(){
   } else {
     alert('Please allow geolocation')
   }
+}
+
+function directions(){
+
+  var destinationPath = []
+  var origin = new google.maps.places.Autocomplete(
+     (document.getElementById('origin')));
+
+  var destination = new google.maps.places.Autocomplete(
+     (document.getElementById('destination')));
+
+  var promise = new Promise(function(resolve, reject){
+    google.maps.event.addListener(origin, 'place_changed', function(){
+      var userOrigin = [origin.getPlace().geometry.location.lat(),origin.getPlace().geometry.location.lng()];
+      resolve(userOrigin)
+    });
+  })
+
+  promise.then(function(data){
+    destinationPath.push(data)
+    console.log(destinationPath)
+  }, function(error){
+    console.log("something went wrong", error)
+  });
+
+  var promise1 = new Promise(function(resolve, reject){
+    google.maps.event.addListener(destination, 'place_changed', function(){
+      var userDestination = [destination.getPlace().geometry.location.lat(),destination.getPlace().geometry.location.lng()];
+      resolve(userDestination)
+    });
+  })
+
+  promise1.then(function(data){
+    destinationPath.push(data)
+    console.log(destinationPath)
+  }, function(error){
+    console.log("something went wrong", error)
+  });
+
+// edge case: someone clicks directions without filling in the start and origin first.
+  $('#directions').click(function(){
+    console.log(destinationPath);
+    // window.location = "http://localhost:3000/directions?variable=" + destinationPath;
+  });
+
+
 }
