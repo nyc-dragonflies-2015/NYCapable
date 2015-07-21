2b319af6caae2ebec3b87de11e9c92e7c0ebd17f
John Doe <benjamin.cohen1989@gmail.com>
2015-07-21 12:17:01 -0400

    added most of direction model that will return station pairs that have no transfer option

--- app/models/direction.rb
+++ app/models/direction.rb
@@ -1,7 +1,22 @@
 class Direction < ActiveRecord::Base
 
-  def index
-
+  def self.getDirections(starting_location, ending_location)
+    no_transfers = []
+    starting_other = []
+    destination_other= []
+    starting_location.each do |start_station|
+      start_station_routes = start_station["station"].routes.pluck(:route_short_name)
+        ending_location.each do |end_station|
+          end_station_routes = end_station["station"].routes.pluck(:route_short_name)
+          if (start_station_routes & end_station_routes).empty? == true
+            starting_other << start_station
+            destination_other << end_station
+          else
+            no_transfers << Hash["start", start_station, "destination", end_station]
+          end
+        end
+    end
+    byebug
   end
 
 end
