e4ee9b2326b67020b1474c7190d428fb0ca31222
Denny Tek <tek.denny@gmail.com>
2015-07-20 20:53:28 -0400

    Pending feature tests

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -2,68 +2,39 @@ require 'rails_helper'
 
 feature 'visit root page' do
 
-  scenario 'user visits root page' do
+  scenario 'user visits root page', js: true   do
     visit root_path
+    save_and_open_page
     expect(page).to have_selector("input#autocomplete")
-
   end
 
   scenario 'user expects input field on root page' do
     visit root_path
     expect(page).to have_selector("input")
-
   end
 
-  xscenario 'user should see station options when submitting address' do
+  xscenario 'user should see station options when submitting address', js:true do
     visit root_path
-<<<<<<< HEAD
-    fill_in "",:with => '48 Wall Street, New York, NY 10015'
-=======
-    fill_in "",:with => '48 Wall Street, New York, NY'
->>>>>>> Remove factory girl from rails_helper
-    expect(page).to redirect_to routes_path
+    fill_in "#autocomplete",:with => '48 Wall Street, New York, NY 10015'
+    expect(page).to redirect_to #routes_path
   end
 
 end
 
-<<<<<<< HEAD
-# :index , :variable=>'40.7063634,-74.00909630000001'
 feature 'visit routes page' do
-  xscenario 'user visits routes page' do
-    # visit('http://localhost:3000/routes?variable=40.7585757,-73.98570899999999')
+  scenario 'user visits routes page' do
+    visit '/routes?variable=40.7585757,-73.98570899999999'
     expect(page).to have_content('Closest stations:')
-     save_and_open_page 
-=======
-
-feature 'visit routes page' do
-  scenario 'user should not see station options on initial visit' do
-    visit root_path
-    expect(page).to have_no_css('div.closest-station-option')
-  end
-end
-
-feature 'visit routes page' do
-  xscenario 'user visits root page' do
-    visit routes_path
-    expect(page).to have_content('Station closest to me:')
->>>>>>> Remove factory girl from rails_helper
   end
 
   xscenario 'user should not see station options on initial visit' do
     visit root_path
-    expect(page).to have_no_css('div.closest-station-option')
+    expect(page).to have_no_css #add selectors
   end
 
-  xscenario 'user should see station options when submitting address' do
-    visit root_path
-    #address input field needs the id 'submit-address-form'
-<<<<<<< HEAD
-    save_and_open_page 
-    fill_in "",:with => '48 Wall Street, New York, NY 10015'
-=======
-    fill_in "",:with => '48 Wall Street, New York, NY'
->>>>>>> Remove factory girl from rails_helper
-    click_button("")
+  scenario 'user should see 5 station options when submitting address' do
+    visit '/routes?variable=40.7585757,-73.98570899999999'
+    expect(page).to have_css('.stationName1')
   end
 
   xscenario 'maps should be hidden until station is clicked' do
@@ -75,28 +46,19 @@ feature 'visit routes page' do
   end
 
   xscenario 'stations should include service status updates' do
+    visit '/routes?variable=40.7585757,-73.98570899999999'
+    expect(page).to have_content('Status')
 
   end
 
-  xscenario 'stations should include distance' do
-<<<<<<< HEAD
-    # visit path
-    expect(page).to have_css('h3.stationName')
-=======
-
->>>>>>> Remove factory girl from rails_helper
+  scenario 'stations should include distance' do
+    visit '/routes?variable=40.7585757,-73.98570899999999'
+    expect(page).to have_content('miles')
   end
-
 end
 
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> Remove factory girl from rails_helper
 feature 'visit other_resource page' do
+
   scenario 'visits other_resource page' do
     visit other_resources_path
     expect(page).to have_text("Other Resources")
@@ -106,23 +68,10 @@ feature 'visit other_resource page' do
     visit other_resources_path
     expect(page).to have_css("img")
   end
-<<<<<<< HEAD
-=======
-
->>>>>>> Remove factory girl from rails_helper
-end
-
-feature 'visit other_resource page' do
 
   scenario 'user sees MTA Resources' do
     visit other_resources_path
     expect(page).to have_content("MTA Resources")
-  end
-
-  xscenario 'user sees revealed MTA Resources links' do
-    visit other_resources_path
-    click_link("MTA Resources")
-    expect(page).to have_content("mta.info | Accessibility")
   end
 
   scenario 'user sees NYC Resources' do
@@ -130,49 +79,16 @@ feature 'visit other_resource page' do
     expect(page).to have_content("NYC Resources")
   end
 
-
-
-  xscenario 'user sees revealed NYC Resources links' do
-    visit other_resources_path
-    find(".nyc-resources").click
-    expect(page).to have_content("NYC Mayor's Office For People with Disabilities")
-  end
-
-  xscenario 'user sees revealed NYC Resources links' do
-    visit other_resources_path
-    click_link("NYC Resources")
-    expect(page).to have_content("mta.info | Accessibility")
-
-  end
-
   scenario 'user sees Government Resources' do
     visit other_resources_path
     expect(page).to have_content("Government Resources")
-  end
-
-  xscenario 'user sees revealed Government Resources links' do
-    visit other_resources_path
-    find(".government-resources").click
-    expect(page).to have_content("ADA.gov")
   end
 
   scenario 'user sees Route Information (pdf)' do
     visit other_resources_path
     expect(page).to have_content("Route Information (pdf)")
   end
-
-
-  xscenario 'user sees revealed MTA Resources links' do
-    visit other_resources_path
-<<<<<<< HEAD
-    find("h2.mta-resources").click
-=======
-    find(".mta-resources").click
->>>>>>> Remove factory girl from rails_helper
-    expect(page).to have_content("mta.info | Accessibility")
-  end
 end
-
 
 feature 'visit About page' do
   scenario 'visits About page' do
@@ -182,39 +98,3 @@ feature 'visit About page' do
 end
 
 
-
-
-    # visit root_path (find a station)
-
-#     expect page text to include "Enter Your Location"
-#     expect page to have geolocation object
-#     (stub four stations to make them appear)
-#     expect page to have a input box powered by Google to input
-#     expect to click the input box, user can enter an address or have location detected
-#     expects to click and four divs appear for each option
-#     expect each div to have a route name and distance
-#     expect to see service status icon in divs
-#       expect to click on div and see map
-
-
-# resources page
-# expect a map
-# expect a list of clickable resources
-# expect separation by boroughs
-
-
-# about page
-# mission
-# what makes us unique
-# includes feedback form?
-
-
-#     expect a nav
-
-#     click_link 'Sign-Up'
-#     fill_in 'Username', :with => 'John'
-#     fill_in 'Email', :with => 'john@example.com'
-#     fill_in 'Password', :with => 'password'
-#     click_button 'Create'
-#     expect(page).to have_content "Logout"
-#   end
