7c6656bcb6765e4b1b0ca7490934195bc4173a6c
John Doe <benjamin.cohen1989@gmail.com>
2015-07-22 13:01:24 -0400

    fixed station model to order the stations by group rather than alphabetical

--- app/models/station.rb
+++ app/models/station.rb
@@ -5,8 +5,12 @@ class Station < ActiveRecord::Base
   def self.print_icons(station)
     @routes = station.routes.pluck(:route_short_name).partition{|x| x.is_a? String}.map(&:sort).flatten
     html = []
-    @routes.each do |x|
-      html << "<span class='mta-bullet mta-#{x.downcase}'>#{x}</span>"
+    @total_routes = ["1","2","3","4","5","6","7","A","C","E","B","D","F","M","G","J","Z","L","S","N","Q","R"]
+    @not_used = @total_routes-@routes
+    @final = @total_routes - @not_used
+    # byebug
+    @final.each do |x|
+      html << "<span class='mta-bullet mta-#{x.downcase}'>#{x}</span>  "
     end
     return html.join.html_safe
   end
