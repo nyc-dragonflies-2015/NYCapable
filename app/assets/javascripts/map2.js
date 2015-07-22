b6f6302c091349d61da5595bbd7e566a7df59b7a
John Doe <benjamin.cohen1989@gmail.com>
2015-07-22 10:47:47 -0400

    updated map2 to user window.user, updated index to clear out extra code, cleaned up routes controller

--- app/assets/javascripts/map2.js
+++ app/assets/javascripts/map2.js
@@ -4,8 +4,7 @@ $(function() {
   var ORDER_OF_MAP_DIVS = ['station1Map', 'station2Map', 'station3Map', 'station4Map', 'station5Map'];
   var ORDER_OF_PANEL_DIVS = ['station1DirectionsPanel', 'bensDirectionsPanel', 'station3DirectionsPanel', 'station4DirectionsPanel', 'station5DirectionsPanel'];
   var ORDER_OF_ELEVATION_DIVS = ['station1Elevation', 'bens_elevation_chart', 'station3Elevation', 'station4Elevation', 'station5Elevation'];
-  var userData = $('.testing').data("user");
-  var userLatLong = new google.maps.LatLng(userData[0], userData[1]);
+  var userLatLong = new google.maps.LatLng(window.user[0], window.user[1]);
 
   $(".panel h3").each(function(i, elem){
     $(elem).on('click', function(e){
